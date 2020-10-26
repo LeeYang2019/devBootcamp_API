07cad6a06f06f075215d77e010b4a7056af5671e
leeyang2019 <nyang@madisoncollege.edu>
2020-10-25 21:35:37 -0400

    added db.js file

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
-// const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-//route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({ path: './config/config.env' });
+
+//connect to db
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -21,6 +25,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-	console.log(`Listening in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(PORT, () =>
+	console.log(
+		`Listening in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
 );
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	server.close(() => process.exit(1));
+});
