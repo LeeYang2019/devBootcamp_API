3db2769c9d8dab53a6aed5a3de57f676f109e3ae
leeyang2019 <nyang@madisoncollege.edu>
2020-10-27 10:38:57 -0400

    created middleware errorHandler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,85 +1,76 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    GET all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = async (req, res, next) => {
-	try {
-		const bootcamps = await Bootcamp.find();
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+	const bootcamps = await Bootcamp.find();
 
-		if (!bootcamps) return res.status(400).json({ success: false });
+	if (!bootcamps) return res.status(400).json({ success: false });
 
-		res
-			.status(200)
-			.json({ success: true, count: bootcamps.length, data: bootcamps });
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+	res
+		.status(200)
+		.json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
 // @desc    GET a bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = async (req, res, next) => {
-	try {
-		//get the bootcamp by its id
-		const bootcamp = await Bootcamp.findById(req.params.id);
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+	//get the bootcamp by its id
+	const bootcamp = await Bootcamp.findById(req.params.id);
 
-		//if bootcamp does not exist...
-		if (!bootcamp) return res.status(400).json({ success: false });
+	//if bootcamp does not exist...
+	if (!bootcamp)
+		return next(
+			new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+		);
 
-		//respond with the bootcamp
-		res.status(200).json({ success: true, data: bootcamp });
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+	//respond with the bootcamp
+	res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc    Create a new bootcamp
 // @route   POST /api/v1/bootcamp
 // @access  Private
-exports.createBootcamp = async (req, res, next) => {
-	try {
-		const bootcamp = await Bootcamp.create(req.body);
-		res.status(201).json({
-			success: true,
-			data: bootcamp,
-		});
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+	const bootcamp = await Bootcamp.create(req.body);
+	res.status(201).json({
+		success: true,
+		data: bootcamp,
+	});
+});
 
 // @desc    Update a bootcamp
 // @route   PUT /api/v1/bootcamp/:id
 // @access  Private
-exports.updateBootcamp = async (req, res, next) => {
-	try {
-		const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true,
-		});
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+	const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
 
-		if (!bootcamp) return res.status(400).json({ success: false });
+	if (!bootcamp)
+		return next(
+			new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+		);
 
-		res.status(200).json({ success: true, data: bootcamp });
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+	res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc    Delete a bootcamp
 // @route   DELETE /api/v1/bootcamp/:id
 // @access  Private
-exports.deleteBootcamp = async (req, res, next) => {
-	try {
-		// get the bootcamp to delete
-		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-		// if the bootcamp does not exist
-		if (!bootcamp) return res.status(400).json({ success: false });
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+	// get the bootcamp to delete
+	const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+	// if the bootcamp does not exist
+	if (!bootcamp)
+		return next(
+			new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+		);
 
-		res.status(200).json({ success: true, data: {} });
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+	res.status(200).json({ success: true, data: {} });
+});
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 //load env vars
@@ -25,6 +26,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // we have to mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+//errorhandler middleware has to come after route
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
