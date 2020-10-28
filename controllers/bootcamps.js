a53f6d954b3ccd9757c8c5a5f4276d290ad78a18
leeyang2019 <nyang@madisoncollege.edu>
2020-10-28 08:29:24 -0400

    created seeder file

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,12 +1,59 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    GET all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-	const bootcamps = await Bootcamp.find();
+	let query;
+
+	//copy req.query
+	const reqQuery = { ...req.query };
+
+	// fields to exclude so it is not looked as a field in the query
+	const removeFields = ['select', 'sort', 'page', 'limit'];
+
+	// loop over removeFields and delete them from reqQuery
+	removeFields.forEach((field) => delete reqQuery[field]);
+
+	//create query string
+	let queryStr = JSON.stringify(reqQuery);
+
+	//create operators
+	queryStr = queryStr.replace(
+		/\b(gt|gte|lt|lte|in)\b/g,
+		(match) => `$${match}`
+	);
+
+	//finding resource
+	query = Bootcamp.find(JSON.parse(queryStr));
+
+	// select fields
+	if (req.query.select) {
+		const fields = req.query.select.split(',').join(' ');
+		query = query.select(fields);
+	}
+
+	//sort
+	if (req.query.sort) {
+		const sortBy = req.query.sort.split(',').join(' ');
+		query = query.sort(sortBy);
+	} else {
+		// return sort in descending order
+		query = query.sort('-createdAt');
+	}
+
+	//pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 100;
+	const skip = (page - 1) * limit;
+
+	query = query.skip(skip).limit(limit);
+
+	//executing query
+	const bootcamps = await query;
 
 	if (!bootcamps) return res.status(400).json({ success: false });
 
@@ -73,4 +120,26 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 		);
 
 	res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamp/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+	const { zipcode, distance } = req.params;
+
+	//get latitude and longitude from geocoder for given zipcode
+	const loc = await geocoder.geocode(zipcode);
+	const lat = loc[0].latitude;
+	const lng = loc[0].longitude;
+
+	const radius = distance / 3963;
+
+	const bootcamps = await Bootcamp.find({
+		location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+	});
+
+	res
+		.status(200)
+		.json({ success: true, count: bootcamps.length, data: bootcamps });
 });
---
+++ seeder.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// load env vars
+dotenv.config({ path: './config/config.env' });
+
+// load models
+const Bootcamp = require('./models/Bootcamp');
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+});
+
+// read json files
+const bootcamps = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// import into db
+const importData = async () => {
+	try {
+		await Bootcamp.create(bootcamps);
+		console.log('Data imported...'.green.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+// delete data
+const deleteData = async () => {
+	try {
+		//delete all bootcamps
+		await Bootcamp.deleteMany();
+		console.log('Data destroyed...'.red.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+if (process.argv[2] === '-i') {
+	importData();
+} else if (process.argv[2] === '-d') {
+	deleteData();
+}
