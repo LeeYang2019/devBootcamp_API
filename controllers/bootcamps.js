51d5c8ff181b78f4f5594633d0f9f190b4f341ad
leeyang2019 <nyang@madisoncollege.edu>
2020-11-03 19:08:49 -0500

    refactored code

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -32,7 +32,23 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamp
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+	//add user to req.body
+	req.body.user = req.user;
+
+	// check for published bootcamp
+	const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+	// if user is not admin, they can only add one bootcamp
+	if (publishedBootcamp && req.user.role !== 'admin')
+		return next(
+			new ErrorResponse(
+				`The user with id ${req.user.id} has already published a bootcamp`,
+				400
+			)
+		);
+
 	const bootcamp = await Bootcamp.create(req.body);
+
 	res.status(201).json({
 		success: true,
 		data: bootcamp,
